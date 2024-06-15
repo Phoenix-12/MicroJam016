06314fa953d4982059b727c7aab714749b402745
Ivan Smorodin <42hackll@gmail.com>
2024-06-15 15:26:12 +0300

    Add bootstrap script, update manifest.json

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    private void Start()
+    {
+        
+    }
+
+    
+    private void Update()
+    {
+        
+    }
+}
