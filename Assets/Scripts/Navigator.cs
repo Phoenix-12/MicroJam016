19ff6026b49c961c25ef131a3b00fcc227d801df
SanditGGprog <150456298+SanditGGprog@users.noreply.github.com>
2024-06-17 00:34:19 +0300

    Update menu, add score, add navigation arrows

--- Assets/Scripts/Navigator.cs
+++ Assets/Scripts/Navigator.cs
@@ -3,34 +3,55 @@ using UnityEngine;
 
 public class Navigator : MonoBehaviour
 {
-    [SerializeField] private GameObject _arrowPrefab; 
-    [SerializeField] private List<GameObject> _arrows = new List<GameObject>();
-    [SerializeField] private List<Transform> _targets; 
+    [SerializeField] private GameObject _arrowPrefab;
+    [SerializeField] private GameObject _bigArrow;
+    [SerializeField] private GameObject _gate;
+    private List<GameObject> _bigArrows = new List<GameObject>();
+    private List<GameObject> _minArrows = new List<GameObject>();
+    [SerializeField] private List<GameObject> _targets = new List<GameObject>(); 
 
     private void Start()
-    {    
-         CreateArrow();
+    {
+        CreateArrow();    
     }
 
+    [System.Obsolete]
     private void FixedUpdate()
     {
-        var i = 0;
-        foreach(var arrow in _arrows)
+        foreach (var bigArrow in _bigArrows)
         {
-            arrow.transform.position = transform.position + (_targets[i].position - transform.position).normalized;
-            arrow.transform.up = (_targets[i].position - transform.position).normalized;
+            bigArrow.transform.position = transform.position + (_gate.transform.position - transform.position).normalized;
+            bigArrow.transform.up = (_gate.transform.position - transform.position).normalized;
+        }
+
+        var i = 0;
+        foreach (var arrow in _minArrows)
+        {
+            if (_targets[i].active)
+            {
+                arrow.transform.position = transform.position + (_targets[i].transform.position - transform.position).normalized;
+                arrow.transform.up = (_targets[i].transform.position - transform.position).normalized;
+                
+            }
+            else
+            {
+                arrow.SetActive(false);
+            }
             i++;
         }
     }
 
     private void CreateArrow()
     {
+        Vector3 bigArrowPosition = transform.position + (_gate.transform.position - transform.position).normalized;
+        var bigArrow = Instantiate(_bigArrow, bigArrowPosition, Quaternion.identity);
+        _bigArrows.Add(bigArrow);
+
         for(int i = 0; i < _targets.Count; i++)
         {
-            Vector3 g = transform.position +(_targets[i].position - transform.position).normalized;
-            var arrow = Instantiate(_arrowPrefab, g, Quaternion.identity);
-            _arrows.Add(arrow);
+            Vector3 minArrowPosition = transform.position +(_targets[i].transform.position - transform.position).normalized;
+            var minArrow = Instantiate(_arrowPrefab, minArrowPosition, Quaternion.identity);
+            _minArrows.Add(minArrow);
         }
     }
-
 }
